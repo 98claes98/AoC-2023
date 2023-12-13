6911df029db079ed78506e35a3e8ce6175504672
Claes Andersson <claes.and98@hotmail.com>
2023-12-13 19:39:32 +0100

    Add project files.

---
+++ Day01/Part1.cs
@@ -0,0 +1,22 @@
+﻿namespace Day01
+{
+    internal static class Part1
+    {
+        internal static void Run()
+        {
+            using var reader = new StreamReader("Input.txt");
+
+            var total = 0;
+
+            while (reader.ReadLine() is string line)
+            {
+                var num1 = line.First(char.IsDigit);
+                var num2 = line.Last(char.IsDigit);
+
+                total += int.Parse(string.Concat(num1, num2));
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Day01/Part2.cs
@@ -0,0 +1,47 @@
+﻿namespace Day01
+{
+    internal static class Part2
+    {
+        internal static void Run()
+        {
+            var calibrationValuesDictionary = new Dictionary<string, string>() {
+                { "one", "1" }, { "two", "2" }, { "three", "3" }, { "four", "4" }, { "five", "5" }, { "six", "6" }, { "seven", "7" }, { "eight", "8" }, { "nine", "9" },
+                { "1", "1" }, { "2", "2" }, { "3", "3" }, { "4", "4" }, { "5", "5" }, { "6", "6" }, { "7", "7" }, { "8", "8" }, { "9", "9" }
+            };
+
+            using var reader = new StreamReader("Input.txt");
+
+            var total = 0;
+
+            while (reader.ReadLine() is string line)
+            {
+                var firstIndex = int.MaxValue;
+                var firstNum = string.Empty;
+
+                var lastIndex = int.MinValue;
+                var lastNum = string.Empty;
+
+                foreach (var keyValuePair in calibrationValuesDictionary)
+                {
+                    var firstIndexInLine = line.IndexOf(keyValuePair.Key);
+                    if (firstIndexInLine != -1 && firstIndexInLine < firstIndex)
+                    {
+                        firstIndex = firstIndexInLine;
+                        firstNum = keyValuePair.Value;
+                    }
+
+                    var lastIndexInLine = line.LastIndexOf(keyValuePair.Key);
+                    if (lastIndexInLine != -1 && lastIndexInLine > lastIndex)
+                    {
+                        lastIndex = lastIndexInLine;
+                        lastNum = keyValuePair.Value;
+                    }
+                }
+
+                total += int.Parse(string.Concat(firstNum, lastNum));
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
